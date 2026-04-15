e04e6a65634764a9c0b9b12347fad2c8d8608d19
Evgen <163992459+evgeniy1331@users.noreply.github.com>
2026-04-15 19:24:58 +0300

    .\.git\

--- src/main/java/org/example/Homework_1.java
+++ src/main/java/org/example/Homework_1.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class Homework_1 {
-    static void main() {
+    public static void main(String[] args) {
         String name = "Иван";
         String jobTitle = "Кассир-шаурмист";
         int ratePerShift = 1000;
